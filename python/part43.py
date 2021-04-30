c78e6999c134dea8451e137ba727d940eae86867
Armon <armonah2@gmail.com>
2021-04-30 09:07:06 +0200

    ORB working:) knock on the table tho.

--- python/part43.py
+++ python/part43.py
@@ -39,9 +39,9 @@ def ORB_matching(img1, img2, threshold = 45):
     good = []
 
     # ratio test as per Lowe's paper
-    for m in matches:
-        if m.distance < threshold:
-            good.append(m)
+    for m in matches[:2000]:
+        # if m.distance < threshold:
+        good.append(m)
 
     p1 = np.array([kp1[m.queryIdx].pt for m in good])
     p2 = np.array([kp2[m.trainIdx].pt for m in good])
@@ -56,7 +56,7 @@ def ORB_matching(img1, img2, threshold = 45):
 
     print(f"Found {len(good)} matches with distance threshold = {threshold}")
 
-    # draw first 200 matches
+    # draw first 2000 matches
     img3 = cv.drawMatches(image1_gray, kp1, image2_gray, kp2, matches[:2000], image2_gray, flags = 2)
     plt.imshow(img3),plt.show()
 
@@ -103,6 +103,53 @@ def generate_model(p1, p2, K, des):
     # print(np.array(inlier_des).shape)
 
     return X, inlier_des, T, uv1, uv2, E
+    
+def bundle_adjustment(T, X, p1, p2, K):
+    if p1.shape[1] == 2:
+        uv1 = np.vstack((p1.T, np.ones(p1.shape[0])))
+        uv2 = np.vstack((p2.T, np.ones(p2.shape[0])))
+    elif p1.shape[0] == 3:
+        uv1 = p1
+        uv2 = p2
+    else:
+        raise "Input points wrong dimentions"
+
+    R0 = T[:3,:3]
+    t0 = T[:3,-1]
+    p0 = np.hstack(([0,0,0], t0, np.ravel(X[:3,:].T)))
+    # print(p0.shape)
+
+    res_func = lambda p: residual(p, R0, K, uv1[:2,:], uv2[:2,:])
+    print(np.mean(np.sqrt(res_func(p0)**2)))
+    sparsity = bundle_adjustment_sparsity(X.shape[1])
+
+    res = least_squares(res_func, p0, verbose=2, jac_sparsity=sparsity, x_scale='jac')
+    p_opt = res['x']
+    print(np.mean(np.sqrt(res_func(p_opt)**2)))
+    #Extracting camera pose and 3d points from bundle adjustment
+    n_points= uv1.shape[1]
+    T_opt = pose(p_opt[:3], p_opt[3:6], R0)
+    X_opt = np.hstack((np.reshape(p_opt[6:], (n_points, 3)), np.ones((n_points,1)))).T
+
+    return T_opt, X_opt
+    
+def pose(angles, translation, R0):
+    ''' Calculates the pose from parametrization'''
+
+    s, c = np.sin, np.cos
+
+    Rx = lambda a: np.array([[1, 0, 0], [0, c(a), s(a)], [0, -s(a), c(a)]])
+    Ry = lambda a: np.array([[c(a), 0, -s(a)], [0, 1, 0], [s(a), 0, c(a)]])
+    Rz = lambda a: np.array([[c(a), s(a), 0], [-s(a), c(a), 0], [0, 0, 1]])
+
+    R = Rx(angles[0]) @ Ry(angles[1]) @ Rz(angles[2]) @ R0
+    t = translation
+
+    T = np.eye(4)
+    T[:3,:3] = R
+    T[:3,3] = t
+
+    return T
 
 def plot_point_cloud(X, uv, img):
     draw_point_cloud(X, img, uv, xlim=[-1.5,+1.5], ylim=[-1.5,+1.5], zlim=[0.5, 3.5])
@@ -128,15 +175,16 @@ if __name__ == "__main__":
 
     X, des, T, uv1, uv2, E = generate_model(p1, p2, K, des)
 
+    T, X = bundle_adjustment(T, X, uv1, uv2, K)
+
     save_model(X, des)
 
     #Plotting results
     img1 = plt.imread("../iCloud Photos/IMG_3980.JPEG")/255.
     img2 = plt.imread("../iCloud Photos/IMG_3981.JPEG")/255.
-    # img1 = plt.imread("../hw5_data_ext/IMG_8207.jpg", cv.IMREAD_GRAYSCALE)/255
-    # img2 = plt.imread("../hw5_data_ext/IMG_8228.jpg", cv.IMREAD_GRAYSCALE)/255
+
     # np.random.seed(123) # Comment out to get a random selection each time
-    # plot_point_cloud(X, uv1, img1)
-    # draw_correspondences(img1, img2, uv1, uv2, F_from_E(E, K), sample_size=8)
+    plot_point_cloud(X, uv1, img1)
+    draw_correspondences(img1, img2, uv1, uv2, F_from_E(E, K), sample_size=8)
     visualize_query_res(X, uv2[:2,:], K, plt.imread("../iCloud Photos/IMG_3981.JPEG"), T)
     plt.show()
